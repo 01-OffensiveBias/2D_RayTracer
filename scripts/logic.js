ee44b9826950d8f4e0e26200a00034cfbe9a3c79
01-OffensiveBias <Darkstar1756@gmail.com>
2016-02-03 09:17:57 -0700

    Final version

--- scripts/logic.js
+++ scripts/logic.js
@@ -1,4 +1,4 @@
-var shapes, lights, rays, debugIntersect = [], debugLowest = [], otherDebugIntersect = [];
+var shapes, lights, rays;
 
 (function () {
     "use strict";
@@ -9,15 +9,27 @@ var shapes, lights, rays, debugIntersect = [], debugLowest = [], otherDebugInter
             this.y = y;
         }
 
+        setIntersectsOutside(intersects) {
+            this.intersectsOutside = intersects;
+        }
+
         distanceTo(other) {
             return Math.sqrt(Math.pow(other.x - this.x, 2) + Math.pow(other.y - this.y, 2));
         }
     }
 
     class LightSource extends Point {
+
+
+
         constructor(x, y, color) {
             super(x, y);
             this.color = color;
+        }
+
+        setVisibleArea(all, visible) {
+            this.all = all;
+            this.visible = visible;
         }
     }
 
@@ -43,9 +55,9 @@ var shapes, lights, rays, debugIntersect = [], debugLowest = [], otherDebugInter
             this.start = p1;
             this.end = p2;
 
-            if (!dir)
-                this.dir = Math.atan(p2.x - p1.x / p2.y - p1.y);
-            if (!length)
+            if (dir)
+                this.dir = dir;
+            if (length)
                 this.length = this.start.distanceTo(this.end);
         }
     }
@@ -53,8 +65,9 @@ var shapes, lights, rays, debugIntersect = [], debugLowest = [], otherDebugInter
     class Ray extends Segment {
         constructor(p1, dir, dest) {
             //super(dir, 2147483647);
+            //dir %= 2 * Math.PI;
             super(p1, new Point(p1.x + Math.cos(dir) * 2147483647, p1.y + Math.sin(dir) * 2147483647), dir);
-            this.dest = dest;
+            this.dest = new Point(dest.x, dest.y);
             // val.start.x + Math.cos(dir) * 2147483647, val.start.y + Math.sin(dir) * 2147483647
         }
     }
@@ -98,7 +111,7 @@ var shapes, lights, rays, debugIntersect = [], debugLowest = [], otherDebugInter
         return calculateLineIntersect(s1.start.x, s1.start.y, s1.end.x, s1.end.y, s2.start.x, s2.start.y, s2.end.x, s2.end.y);
     }
 
-    // Returns a Point if the lines intersect, otherwise null. In addition, if the lines
+    // Thanks StackOverflow!
     function calculateLineIntersect(p0_x, p0_y, p1_x, p1_y, p2_x, p2_y, p3_x, p3_y) {
         var s1_x, s1_y, s2_x, s2_y;
         s1_x = p1_x - p0_x;
@@ -110,129 +123,106 @@ var shapes, lights, rays, debugIntersect = [], debugLowest = [], otherDebugInter
         s = (-s1_y * (p0_x - p2_x) + s1_x * (p0_y - p2_y)) / (-s2_x * s1_y + s1_x * s2_y);
         t = ( s2_x * (p0_y - p2_y) - s2_y * (p0_x - p2_x)) / (-s2_x * s1_y + s1_x * s2_y);
 
-        if (s >= 0 && s <= 1.05 && t >= 0 && t <= 1.05) {
-            // Collision detected
+        if (s >= 0 && s <= 1 && t >= 0 && t <= 1) {
             var i_x = p0_x + (t * s1_x);
             var i_y = p0_y + (t * s1_y);
             return new Point(i_x, i_y);
         }
 
-        return null; // No collision
+        return null;
     }
 
     lights = [
-        new LightSource(200, 250, "yellow")
+        //new LightSource(650, 500, "yellow"),
+       /** new LightSource(500, 300, "rgba(244, 67, 54, 0.5)"),
+        new LightSource(515, 300, "rgba(255,255,255,0.2)"),
+        new LightSource(485, 300, "rgba(255,255,255,0.2)"),
+        new LightSource(500, 315, "rgba(255,255,255,0.2)"),
+        new LightSource(500, 285, "rgba(255,255,255,0.2)"),
+        new LightSource(515, 315, "rgba(255,255,255,0.2)"),
+        new LightSource(515, 285, "rgba(255,255,255,0.2)"),
+        new LightSource(485, 315, "rgba(255,255,255,0.2)"),*/
+        new LightSource(485, 285, "rgba(255,255,255,0.2)")
     ];
 
     shapes = [
         new Rectangle(0, 0, 750, 750),
         new Rectangle(30, 30, 80, 80),
-        new Rectangle(400, 80, 60, 120)
+        new Rectangle(400, 80, 60, 120),
+        new Rectangle(300, 550, 350, 50),
     ];
 
-    rays = [];
-
     document.querySelector('#canvas').onmousemove = function (e) {
-        rays = [];
+        update(e);
+    };
+    update(null);
+    function update(e) {
+        if (e != null) {
+            lights[0].x = e.clientX;
+            lights[0].y = e.clientY;
+        }
 
         lights.forEach(function (light) {
-            light.x = e.clientX;
-            light.y = e.clientY;
+            rays = [];
 
             shapes.forEach(function (shape) {
                 shape.points.forEach(function (point) {
-                    rays.push(new Ray(new Point(light.x, light.y), Math.atan2(light.y - point.y, light.x - point.x) - Math.PI, point));
+                    rays.push(new Ray(new Point(light.x, light.y), Math.atan2(light.y - point.y, light.x - point.x) + Math.PI - .01, point));
+                    rays.push(new Ray(new Point(light.x, light.y), Math.atan2(light.y - point.y, light.x - point.x) + Math.PI,       point));
+                    rays.push(new Ray(new Point(light.x, light.y), Math.atan2(light.y - point.y, light.x - point.x) + Math.PI + .01, point));
                 });
             });
-        });
 
-        rays.sort(function (a, b) {
-            return a.dir - b.dir;
-        });
+            rays.sort(function (a, b) {
+                return -a.dir + b.dir;
+            });
 
-        //console.log(rays);
+            var allIntersects = []; // TODO: remove, used for debug rendering only
+            var intersects = [];
 
-        debugIntersect = [];
-        debugLowest = [];
-        otherDebugIntersect = [];
+            rays.forEach(function (ray, rayIndex) {
 
-        var light = lights[0];
+                var lowest = null;
+                shapes.forEach(function (shape, shapeIndex) {
+                    shape.segments.forEach(function (segment) {
+                        var intersect = getLineIntersect(ray, segment);
 
+                        if (intersect != null && (/*shapeIndex == 0 ||*/
+                            intersect.distanceTo(segment.start) > 0.5 &&
+                            intersect.distanceTo(segment.end) > 0.5)) {
 
-        rays.forEach(function (ray) {
-            var intersections = [];
-            var lowest = null;
-            /*shapes.forEach(function (shape) {
-                intersections.concat(shape.points);
-            });*/
-            intersections.push(ray.dest);
+                            //intersect.setIntersectsOutside(shapeIndex == 0);
 
+                            if (lowest == null) {
+                                lowest = intersect;
+                            } else if (intersect.distanceTo(light) < lowest.distanceTo(light)) {
+                                lowest = intersect;
+                            }
 
-            shapes.forEach(function (shape) {
-                shape.segments.forEach(function (segment) {
-                    var intersect = getLineIntersect(ray, segment);
-
-                    if (intersect != null
-                        /*intersect.distanceTo(segment.start) > 0.5 &&
-                        intersect.distanceTo(segment.end) > 0.5*/) {
-
-                        if (lowest == null) {
-                            lowest = intersect;
-                        } else if (intersect.distanceTo(light) < lowest.distanceTo(light)) {
-                            lowest = intersect;
+                            allIntersects.push(intersect);
                         }
-
-                        intersections.push(intersect);
-                        debugIntersect.push(intersect);
-                    }
+                    });
                 });
 
-
-
+                //TODO: fix
+                if (lowest != null) {
+                    // Works most of the time
+                   if ((rayIndex - 1) % 3 == 0 && lowest.distanceTo(light) > ray.dest.distanceTo(light)) {
+                        intersects.push(ray.dest)
+                    } else {
+                        intersects.push(lowest);
+                    }
+                } else {
+                    if (shapes[0].segments.indexOf(ray.dest)) {
+                        intersects.push(ray.dest);
+                    }
+                }
             });
 
-            if (lowest != null) {
-                debugLowest.push(lowest);
-            }
-            //debugLowest.push(ray.dest);
+            light.setVisibleArea(allIntersects, intersects);
         });
-    };
+    }
 
 })();
 
 
-/* var light = lights[0];
- shapes.forEach(function (shape) {
- shape.segments.forEach(function (segment) {
-
- var intersections = [];
-
- rays.forEach(function (ray) {
- var intersect = getLineIntersect(ray, segment);
-
- var lowest = null;
-
- if (intersect != 0 && intersect != null &&
- intersect.distanceTo(segment.start) > 0.5 &&
- intersect.distanceTo(segment.end) > 0.5) { // Null check should not be needed
-
- if (lowest == null) {
- lowest = intersect;
- } else if (intersect.distanceTo(light) < lowest.distanceTo(light)) {
- lowest = intersect;
- }
- }
-
- if (lowest != null) {
- debugLowest.push(lowest);
- intersections.push(intersect);
- debugIntersect.push(intersect);
- }
- });
-
- shapes.forEach(function (shape) {
- intersections.concat(shape.points);
- });
- });
- });
- };*/
--- scripts/render.js
+++ scripts/render.js
@@ -8,69 +8,96 @@ document.addEventListener("DOMContentLoaded", function () {
         ctx.clearRect(0, 0, 750, 750);
 
         // Draw shapes
+        ctx.fillStyle = 'teal';
         shapes.forEach(function (val, ndx, arr) {
-            var points = val.points;
+            if (ndx != 0) {
+                var points = val.points;
 
-            ctx.beginPath();
-            for (var i = 0; i < points.length - 1; i++) {
-                ctx.moveTo(points[i].x, points[i].y);
-                ctx.lineTo(points[i + 1].x, points[i + 1].y);
+                ctx.beginPath();
+                /* for (var i = 0; i < points.length - 1; i++) {
+                 ctx.moveTo(points[i].x, points[i].y);
+                 ctx.lineTo(points[i + 1].x, points[i + 1].y);
+                 }*/
+
+                //ctx.moveTo(points[0].x, points[0].y);
+                for (var i = 0; i < points.length; i++) {
+                    ctx.lineTo(points[i].x, points[i].y);
+                }
+                //ctx.lineTo(points[0].x, points[0].y);
+
+                ctx.fill();
+                ctx.closePath();
             }
 
-            ctx.lineTo(points[0].x, points[0].y);
-            ctx.stroke();
-            ctx.closePath();
         });
 
-        lights.forEach(function (val, ndx, arr) {
+        ctx.fillStyle = 'black';
+
+
+        // Red lines
+        /*ctx.strokeStyle = 'red';
+         ctx.beginPath();
+         for (var i = 0 ; i < light.visible.length - 1; i++) {
+         ctx.moveTo(light.visible[i].x,            light.visible[i].y);
+         ctx.lineTo(light.visible[i + 1].x,        light.visible[i + 1].y);
+         ctx.stroke();
+         }
+         ctx.closePath();*/
+        lights.forEach(function (light) {
+
+            // Draw the triangles
+            // TODO: cleanup
+            ctx.fillStyle = light.color;
+            for (var i = 0; i < light.visible.length - 1; i++) {
+                ctx.beginPath();
+                ctx.moveTo(light.x, light.y);
+                ctx.lineTo(light.visible[i].x, light.visible[i].y);
+                ctx.lineTo(light.visible[i + 1].x, light.visible[i + 1].y);
+                ctx.fill();
+                ctx.closePath();
+            }
             ctx.beginPath();
-            ctx.arc(val.x, val.y, 20, 0, 2 * Math.PI);
-            ctx.fillStyle = val.color;
+            ctx.moveTo(light.x, light.y);
+            ctx.lineTo(light.visible[light.visible.length - 1].x, light.visible[light.visible.length - 1].y);
+            ctx.lineTo(light.visible[0].x, light.visible[0].y);
             ctx.fill();
             ctx.closePath();
         });
 
-        rays.forEach(function (val, ndx, arr) {
+        ctx.fillStyle = 'yellow';
+        lights.forEach(function (light) {
+            // Draw light source
             ctx.beginPath();
-            ctx.moveTo(val.start.x, val.start.y);
-            ctx.lineTo(val.end.x, val.end.y);
-            ctx.stroke();
+            ctx.arc(light.x, light.y, 20, 0, 2 * Math.PI);
+            ctx.fillStyle = light.color;
+            ctx.fill();
             ctx.closePath();
+
+            /*// All ray intersects
+             ctx.fillStyle = 'green';
+             light.allIntersects.forEach(function (point) {
+             if (point != null) {
+             ctx.beginPath();
+             ctx.arc(point.x, point.y, 3, 0, 2 * Math.PI);
+             ctx.fill();
+             ctx.closePath();
+             }
+             });
+
+             // Only visible intersects
+             ctx.fillStyle = 'blue';
+             light.visible.forEach(function (point) {
+             if (point != null) {
+             ctx.beginPath();
+             ctx.arc(point.x, point.y, 3, 0, 2 * Math.PI);
+             ctx.fill();
+             ctx.closePath();
+             }
+             });*/
         });
-
-        debugIntersect.forEach(function (point) {
-            if (point != null) {
-                ctx.beginPath();
-                ctx.fillStyle = 'green';
-                ctx.arc(point.x, point.y, 3, 0, 2 * Math.PI);
-                ctx.fill();
-                ctx.closePath();
-            }
-        });
-
-        //otherDebugIntersect.forEach(function (point) {
-        //    if (point != null) {
-        //        ctx.beginPath();
-        //        ctx.fillStyle = 'blue';
-        //        ctx.arc(point.x, point.y, 3, 0, 2 * Math.PI);
-        //        ctx.fill();
-        //        ctx.closePath();
-        //    }
-        //});
-
-        debugLowest.forEach(function (point) {
-            if (point != null) {
-                ctx.beginPath();
-                ctx.fillStyle = 'blue';
-                ctx.arc(point.x, point.y, 3, 0, 2 * Math.PI);
-                ctx.fill();
-                ctx.closePath();
-            }
-        });
-
-        window.webkitRequestAnimationFrame(draw);
+        window.requestAnimationFrame(draw);
     }
 
-    window.webkitRequestAnimationFrame(draw);
+    window.requestAnimationFrame(draw);
 
 });
